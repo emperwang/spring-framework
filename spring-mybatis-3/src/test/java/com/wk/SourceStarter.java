2818e0d1b1dc8a69df13f88e91b15d07603ec571
emperWang <544094478@qq.com>
2020-05-15 20:58:39 +0800

    add mybatis-3 source

---
+++ spring-mybatis-3/src/test/java/com/wk/SourceStarter.java
@@ -0,0 +1,26 @@
+package com.wk;
+
+import com.wk.entity.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class SourceStarter {
+
+  @Test
+  public void sources() throws IOException {
+    SqlSessionFactory sessionFactory;
+    Reader reader = Resources.getResourceAsReader("MyBatis-config.xml");
+    // 创建sqlSessionFactory,在这里对xml文件进行了解析
+    sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+    SqlSession sqlSession = sessionFactory.openSession();
+    UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+    User user = mapper.selectById(1);
+    System.out.println(user.toString());
+  }
+}
---
+++ spring-mybatis-3/src/test/java/com/wk/entity/User.java
@@ -0,0 +1,24 @@
+package com.wk.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+public class User {
+  private int id;
+  private String name;
+  private int age;
+  private String address;
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", age=" + age +
+				", address='" + address + '\'' +
+				'}';
+	}
+}
